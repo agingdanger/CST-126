1b2910b9de0cf7ea719893c378c939e386205722
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-15 19:04:24 -0700

    My first commit

---
+++ insert (1).php
@@ -0,0 +1,40 @@
+
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Preston, Charles, Kevin, Filiberto
+ * Date: 9/9/2018
+ * Time: 3:42 PM
+ */
+
+$port = 3306;
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+if (!empty($username) && !empty($password) && !empty($email)) {
+
+    @$db = mysqli_connect("localhost", "root", "root", "test");
+}else {
+    echo "All values must be entered";
+    exit("1");
+}
+mysqli_select_db("\mysqli", "test");
+{
+
+    if (mysqli_connect_errno()) {
+        echo "Error: Could not connect to database.  Please try again later.";
+    }else{
+        echo "Connection successful ";
+
+    }
+
+    $sql = "INSERT INTO users (userName,userPass,userEmail) VALUES ('$username', '$password', '$email')";
+
+    if(mysqli_query($db, $sql)){
+        echo "Records inserted successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+    }
+}
