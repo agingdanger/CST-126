518074f5f01d68ba3b34cd550aeaa8b6fa8248d8
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-23 16:56:09 -0700

    Changed connectivity.php to correct userPass variable

--- connectivity.php
+++ connectivity.php
@@ -25,10 +25,11 @@ function SignIn()
     if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
     {
         $query =("SELECT *  FROM user where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-        $row =($query) or die();
-        if(!empty($row['userName']) AND !empty($row['pass']))
+        $row = mysqli_query($query) or die();
+		//$conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
+        if(!empty($row['userName']) AND !empty($row['userPass']))
         {
-            $_SESSION['userName'] = $row['userName'];
+            $_SESSION['userName'] = $row['userPass'];
             echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
 
         }
