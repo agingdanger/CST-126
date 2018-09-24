d78f46ad90fad6dbf52a2494bf843447eb0381c7
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-23 17:10:45 -0700

    reloaded connectivity.php because i messed something up, starting over

--- connectivity.php
+++ connectivity.php
@@ -15,22 +15,20 @@ if (mysqli_connect_errno()) {
 
 }
 
-
+/*
 $ID = $_POST['user'];
 $Password = $_POST['pass'];
-
+*/
 function SignIn()
 {
     session_start();   //starting the session for user profile page
     if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
     {
         $query =("SELECT *  FROM user where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-        $row = mysqli_query($query) or die();
-		$count = mysql_num_rows($row);
-		//$conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
-        //if(!empty($row['user']) AND !empty($row['user']))
+        $row =($query) or die();
+        if(!empty($row['userPass']) AND !empty($row['pass']))
         {
-            $_SESSION['user'] = $row['user'];
+            $_SESSION['userName'] = $row['pass'];
             echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
 
         }
