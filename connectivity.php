fda182a64d4cae1bff4eb2e31c1c521467fc4b82
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-26 15:18:27 -0700

    did more

--- connectivity.php
+++ connectivity.php
@@ -15,8 +15,8 @@ if (mysqli_connect_errno($db)) {
 
 }
 
-echo $_POST['user'];
-echo $_POST['pass'];
+//echo $_POST['user'];
+//echo $_POST['pass'];
 
 $ID = $_POST['user'];
 $Password = $_POST['pass'];
@@ -26,37 +26,39 @@ $start = true;
 
 //function SignIn()
 //{
-    session_start();   //starting the session for user profile page
-    //if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
-    if($start)
-    {
-        //echo "you made it this far";
-        
-        $query = mysqli_query($db, "SELECT *  FROM user where userName = '$ID' AND userPass = '$Password'");// or die(mysqli_error());
-        $result = mysqli_query($db, $query);
-        if ( false == $result){
-            echo "error: ", mysqli_error($query);
-        }
-        //echo "but not this far";
-        echo $query;
-        
-        $row =($query); //or die();
-        echo "how about this";
-        if(!empty($row['userName']) AND !empty($row['userPass']))
-        {
-            $_SESSION['userName'] = $row['pass'];
-            echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
 
+        session_start();   //starting the session for user profile page
+        //if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+        if($start)
+        {
+            //echo "you made it this far";
+            
+            $query = mysqli_query($db, "SELECT *  FROM user where userName = '$ID' AND userPass = '$Password'");// or die(mysqli_error());
+            $result =$query;
+            if ( false == $result){
+                echo "error: ", mysqli_error($query);
+            }
+            //echo "but not this far";
+            echo $result;
+            
+            $row =($query); //or die();
+            echo "how about this";
+            if(!empty($row['userName']) AND !empty($row['userPass']))
+            {
+                $_SESSION['userName'] = $row['pass'];
+                echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+    
+            }
+            else
+            {
+                echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+            }
         }
         else
         {
-            echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+            echo "You suck.";
         }
-    }
-    else
-    {
-        echo "You suck.";
-    }
+
 //}
 //if(isset($_POST['submit']))
 //{
