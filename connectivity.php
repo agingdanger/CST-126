495e5dee97afe8fb4483f626bb87f00a1f905cd4
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-26 14:17:23 -0700

    changed the connectivity file sign-in function a little bit

--- connectivity.php
+++ connectivity.php
@@ -8,23 +8,26 @@
 
 @$db = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
 
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno($db)) {
     echo "Error: Could not connect to database.  Please try again later.";
 }else{
     echo "Connection successful ";
 
 }
 
-/*
-$ID = $_POST['user'];
-$Password = $_POST['pass'];
-*/
+
+//$ID = $_POST['user'];
+//$Password = $_POST['pass'];
+
+
+SignIn();
+
 function SignIn()
 {
     session_start();   //starting the session for user profile page
     if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
     {
-        $query =("SELECT *  FROM user where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
+        $query =mysql_query("SELECT *  FROM user where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
         $row =($query) or die();
         if(!empty($row['userName']) AND !empty($row['pass']))
         {
@@ -38,7 +41,7 @@ function SignIn()
         }
     }
 }
-if(isset($_POST['submit']))
-{
-    SignIn();
-}
+//if(isset($_POST['submit']))
+//{
+//    SignIn();
+//}
