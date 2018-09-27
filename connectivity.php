fa5f7a6e1607fe40906daaa636751340748a239c
Charles Henderson <CHenderson33@my.gcu.edu>
2018-09-26 17:34:53 -0700

    idk

--- connectivity.php
+++ connectivity.php
@@ -22,9 +22,9 @@ $Password = $_POST['pass'];
 function SignIn()
 {
     session_start();   //starting the session for user profile page
-    if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+    if(!empty($_POST['userr']))   //checking the 'user' name which is from Sign-In.html, is it empty or have some text
     {
-        $query =("SELECT *  FROM user where userName = '$_POST[userr]' AND userPass = '$_POST[pass]'") or die(mysql_error());
+        $query =("SELECT *  FROM user where userName = '$_POST[userr]' AND pass = '$_POST[pass]'") or die(mysql_error());
         $row =($query) or die();
         if(!empty($row['userName']) AND !empty($row['pass']))
         {
