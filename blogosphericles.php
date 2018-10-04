7851580f2b0baa22c0af7c63663cd7422a44368e
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-03 22:47:56 -0700

    made changes so that code is in better order

--- blogosphericles.php
+++ blogosphericles.php
@@ -12,12 +12,14 @@ $post = $_POST['post'];
 if(!empty($post)){
     $conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
     
+    $sql = "INSERT INTO blogPost (post) VALUES ('$post')";
+    
 }else {
     echo "You need to type something in!";
     exit("1");
 }
 
-$sql = "INSERT INTO blogPost (post) VALUES ('$post')";
+
 
 if(mysqli_query($conn, $sql)){
     echo "Blog Posted to our Records";
