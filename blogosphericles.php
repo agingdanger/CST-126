b23df6c67e80c6c6e661e84d622bc92447c813ab
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-03 23:41:34 -0700

    changed textarea guts

--- blogosphericles.php
+++ blogosphericles.php
@@ -1,10 +1,11 @@
-
 <?php
 $post = $_POST['comment'];
 
 
 if(!empty($post)){
+    
     echo $post;
+    
     $conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
     
     $sql = "INSERT INTO blogPost (pos) VALUES ('$post')";
