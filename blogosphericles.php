52a74db5b617ba38108926838dc7de6985ec4a9d
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-05 22:17:50 -0700

    wow figured it out kinda, just need to be sure i'm getting the correct info from session variables

--- blogosphericles.php
+++ blogosphericles.php
@@ -1,7 +1,7 @@
 <?php
 $post = $_POST['comment'];
 
-//$userID = mysql_real_escape_string($_SESSION['id']);
+$userID = mysql_real_escape_string($_SESSION['id']);
 
 //echo $_SESSION['userr'];
 
@@ -16,7 +16,7 @@ if(!empty($post)){
     
     $conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
     
-    $sql = ("INSERT INTO blogPost (postId, pos, user_userID) VALUES ( '', '$post' , '1')");
+    $sql = ("INSERT INTO blogPost (postId, pos, user_userID) VALUES ( '', '$post' , '$userID')");
     
 } else {
     echo "You need to type something in!";
