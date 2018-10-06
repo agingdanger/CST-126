e149ef7fd397b58ed46d0d6b12237b3c4ec2ba95
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-05 22:14:29 -0700

    try again

--- blogosphericles.php
+++ blogosphericles.php
@@ -16,7 +16,7 @@ if(!empty($post)){
     
     $conn = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "g7t9d2srsz60d6e8", "peqn2hgv8zm6awzt", "lhhymmozru2i72c4");
     
-    $sql = ("INSERT INTO blogPost (postId, pos, user_userID) VALUES ( '', '$post' , '{$_SESSION['id']}')");
+    $sql = ("INSERT INTO blogPost (postId, pos, fk_blogPost_user_idx) VALUES ( '', '$post' , '{$_SESSION['id']}')");
     
 } else {
     echo "You need to type something in!";
@@ -28,7 +28,7 @@ if(!empty($post)){
 if(mysqli_query($conn, $sql)){
     echo "Blog Posted to our Records";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($sql);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
         
         
