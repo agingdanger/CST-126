934533e7b84c5fb5f6d0c0eb15434441f990746d
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-05 23:55:25 -0700

    can't add files?

---
+++ content.php
@@ -0,0 +1,22 @@
+<?php
+  session_start();  
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Secret Page</title>
+</head>
+<body>
+<h1>Here it is!</h1>
+<p>Post up.</p>
+<form action="blogosphericles.php"  method="POST" >
+<textarea  id = "comment" name="comment" rows="35" cols="80">Say something</textarea>
+			 <br>
+        Share<br></label>
+        <input type="submit" value="->">
+</form>
+</body>
+</html>
