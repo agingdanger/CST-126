f6b074910f5b3d8ccf36ef33ddba28fe764bc140
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-05 21:33:19 -0700

    changed the session store method again

--- connectivity.php
+++ connectivity.php
@@ -71,13 +71,33 @@ function SignIn()
         //if(!empty($row['userName']) && !empty($row['userPass']))
 
         {
-            while($row = mysqli_fetch_array($result)){
-                
-                $_SESSION[id] =  $row['userID'];
-                $_SESSION["user"] = $row['userr'];
-                $_SESSION["pass"] = $row['pass'];
-                $_SESSION["email"] = $row['userEmail'];
+            if($result = mysqli_query($conn, $query)){
+                if(mysqli_num_rows($result) > 0){
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>id</th>";
+                    echo "<th>username</th>";
+                    echo "<th>password</th>";
+                    echo "<th>email</th>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_array($result)){
+                        echo "<tr>";
+                        $_SESSION[id] =  $row['userID'];
+                        $_SESSION["user"] = $row['userr'];
+                        $_SESSION["pass"] = $row['pass'];
+                        $_SESSION["email"] = $row['userEmail'];
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                    // Free result set
+                    mysqli_free_result($result);
+                } else{
+                    echo "No records matching your query were found.";
+                }
             }
+                
+               
+            
             //$_SESSION["user"] = $_POST['userr'];
             //$_SESSION["pass"] = $_POST['pass'];
             //$_SESSION["id"] = $_POST['userID'];
