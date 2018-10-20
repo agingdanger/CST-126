99469740ff9f0c0443a426c99b69538f40583418
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-20 14:56:34 -0700

    Whats going on

---
+++ InClass.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <meta charset="UTF-8">
+</head>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smith
+ * Date: 10/2/2018
+ * Time: 5:18 PM
+ */
+Function reverse_r ($str) {
+
+    echo substr($str, 0, -4);
+}
+
+function reverse_i ($str) {
+    {
+        echo substr($str, 0, -4);
+    }
+    return;
+}
+
+reverse_r('Hello');
+echo '<br />';
+reverse_i('Bananas');
+
+
+
