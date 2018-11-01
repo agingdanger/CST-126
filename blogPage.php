5efad78e598cc217236b3708286f97594104d5b3
Charles Henderson <CHenderson33@my.gcu.edu>
2018-10-31 20:12:30 -0700

    combined comment section and blogpage

--- blogPage.php
+++ blogPage.php
@@ -89,3 +89,29 @@ echo "<br>";
         //}
 
 ?>
+
+
+<?php
+session_start();
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <title>Secret Page</title>
+</head>
+<body>
+<h1>Here it is!</h1>
+<p>Post up.</p>
+<form action="blogosphericles.php"  method="POST" >
+    <textarea  id = "comment" name="comment" rows="35" cols="80">Say something</textarea>
+    <br>
+    Share<br></label>
+    <input type="submit" value="->">
+</form>
+</body>
+</html>
+
