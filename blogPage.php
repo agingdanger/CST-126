5e45e21eaa3409c9be41fb0ce76ffcf50bdd4dd0
DarthRevan75 <43393601+DarthRevan75@users.noreply.github.com>
2018-11-08 17:45:37 -0700

    Tried search bar

--- blogPage.php
+++ blogPage.php
@@ -2,10 +2,15 @@
 <head>
     <link rel="stylesheet" type="text/css" href="mystyle.css">
     <meta charset="UTF-8">
-</head>
+<body>
+<h1>Here it is!</h1>
+</body>
+<div class="topnav">
+    <input type="text" placeholder="Search..">
+</div>
 </html>
 
-<?php 
+<?php
 
 session_start();
 
@@ -15,53 +20,63 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$DPost = $_POST["deleet"];
+
+$result = mysqli_query($conn, $sql);
+
+//"SELECT FROM blogPost WHERE postID == $DPost AND user_userID == $_SESSION('id') LIMIT 1"
+
+if($sql = "SELECT FROM blogPost WHERE postID = $DPost AND user_userID = $_SESSION('id')"){
+    //if($sql = "SELECT FROM blogPost WHERE user_userID = $_SESSION('id') && SELECT FROM user WHERE userID = $_SESSION('id')"){
+    $sql = "DELETE FROM blogPost WHERE postID = $DPost";
+    if (mysqli_query($conn, $sql)) {
+        //echo "fourth if";
+    }
+}
+
+
 $sql = "SELECT * FROM blogPost";
 
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
+	echo "<table>";
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo $row["postID"]. " - " . $row["pos"]. " " . $row["user_userID"]. "<br>";
+		echo "<tr>";
+        echo "<td>" . $row["postID"]. " - " . $row["pos"]. " " . $row["user_userID"] . "</td>";
         
-        //if($row["user_userID"] === $_SESSION['id']){
-        //    
-        
-        //}
-        
+		if($row["user_userID"] === $_SESSION['id'] || $_SESSION["admin"] == 1){
+        ?>  
+			
+            <form action = "blogPage.php" method = "POST">
+			<td>
+				<input id = "delete" type = "submit" name = "delete_post" value = "Delete">
+			</td>
+			<input id = "deleet" type = "hidden" name = "deleet" value = "<?= $row['postID'] ?>">
+            </form>
+
+            <form action = "editPage.php" method = "POST">
+                <td>
+                    <input id = "edit" type = "submit" name = "edit" value = "Edit Post">
+                </td>
+                <input id = "edit" type = "hidden" name = "editt" value = "<?= $row['postID'] ?>">
+                <input id = "edit" type = "hidden" name = "act" value = "0">
+            </form>
+		
+		<?php
+        }
+        echo "</tr>";
     }
-} 
+	echo "</table>";
+}
 else 
 {
 echo "0 results";
 }
-    ?>
-            <html>
-            <form action = "blogPage.php" method = "POST">
-            <input id = "delete" type = "submit" name = "delete_post" value = "Delete">
-            Delete A Post
-            <input id = "deleet" type = "text" name = "deleet" value = "Post ID">
-            </form>
-            </html>
-            <?php
+echo "<br>";
             
-            session_start();
-            
-            $DPost = $_POST["deleet"];
-            
-            
-            
-            $result = mysqli_query($conn, $sql);
-            
-            //"SELECT FROM blogPost WHERE postID == $DPost AND user_userID == $_SESSION('id') LIMIT 1"
-    
-            if($sql = "SELECT FROM blogPost WHERE postID = $DPost AND user_userID = $_SESSION('id')"){
-                //if($sql = "SELECT FROM blogPost WHERE user_userID = $_SESSION('id') && SELECT FROM user WHERE userID = $_SESSION('id')"){
-                $sql = "DELETE FROM blogPost WHERE postID = $DPost";
-                if (mysqli_query($conn, $sql)) {
-                    echo "fourth if";
-                }
-            }
+
             
             
         //if  (mysqli_num_rows($result) > 0) {
@@ -86,3 +101,28 @@ echo "0 results";
         //}
 
 ?>
+
+
+<?php
+session_start();
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <title>Secret Page</title>
+</head>
+<body>
+<h1>Post Up!</h1>
+<form action="blogosphericles.php"  method="POST" >
+    <textarea  id = "comment" name="comment" rows="35" cols="80">Say something</textarea>
+    <br>
+    Share<br></label>
+    <input type="submit" value="->">
+</form>
+</body>
+</html>
+
