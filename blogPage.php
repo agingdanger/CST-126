3c07aae3a0fe01eed12bfeffa9b196393797f8c8
DarthRevan75 <43393601+DarthRevan75@users.noreply.github.com>
2018-11-08 18:02:11 -0700

    search & mod

--- blogPage.php
+++ blogPage.php
@@ -1,84 +1,87 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="mystyle.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        * {box-sizing: border-box;}
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            * {box-sizing: border-box;}
 
-        body {
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .topnav {
-            overflow: hidden;
-            background-color: #e9e9e9;
-        }
-
-        .topnav a {
-            float: left;
-            display: block;
-            color: black;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-size: 17px;
-        }
-
-        .topnav a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        .topnav a.active {
-            background-color: #2196F3;
-            color: white;
-        }
-
-        .topnav .search-container {
-            float: right;
-        }
-
-        .topnav input[type=text] {
-            padding: 6px;
-            margin-top: 8px;
-            font-size: 17px;
-            border: none;
-        }
-
-        .topnav .search-container button {
-            float: right;
-            padding: 6px;
-            margin-top: 8px;
-            margin-right: 16px;
-            background: #ddd;
-            font-size: 17px;
-            border: none;
-            cursor: pointer;
-        }
-
-        .topnav .search-container button:hover {
-            background: #ccc;
-        }
-
-        @media screen and (max-width: 600px) {
-            .topnav .search-container {
-                float: none;
-            }
-            .topnav a, .topnav input[type=text], .topnav .search-container button {
-                float: none;
-                display: block;
-                text-align: left;
-                width: 100%;
+            body {
                 margin: 0;
-                padding: 14px;
+                font-family: Arial, Helvetica, sans-serif;
             }
+
+            .topnav {
+                overflow: hidden;
+                background-color: #e9e9e9;
+            }
+
+            .topnav a {
+                float: left;
+                display: block;
+                color: black;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                font-size: 17px;
+            }
+
+            .topnav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            .topnav a.active {
+                background-color: #2196F3;
+                color: white;
+            }
+
+            .topnav .search-container {
+                float: right;
+            }
+
             .topnav input[type=text] {
-                border: 1px solid #ccc;
+                padding: 6px;
+                margin-top: 8px;
+                font-size: 17px;
+                border: none;
             }
-        }
-    </style>
-</head>
+
+            .topnav .search-container button {
+                float: right;
+                padding: 6px;
+                margin-top: 8px;
+                margin-right: 16px;
+                background: #ddd;
+                font-size: 17px;
+                border: none;
+                cursor: pointer;
+            }
+
+            .topnav .search-container button:hover {
+                background: #ccc;
+            }
+
+            @media screen and (max-width: 600px) {
+                .topnav .search-container {
+                    float: none;
+                }
+                .topnav a, .topnav input[type=text], .topnav .search-container button {
+                    float: none;
+                    display: block;
+                    text-align: left;
+                    width: 100%;
+                    margin: 0;
+                    padding: 14px;
+                }
+                .topnav input[type=text] {
+                    border: 1px solid #ccc;
+                }
+            }
+        </style>
+    </head>
 <body>
 
 <div class="topnav">
@@ -96,6 +99,7 @@
     <p>Resize the browser window to see the responsive effect.</p>
 </div>
 
+</html>
 </html>
 
 <?php
@@ -204,7 +208,8 @@ session_start();
     <title>Secret Page</title>
 </head>
 <body>
-<h1>Post Up!</h1>
+<h1>Here it is!</h1>
+<p>Post up.</p>
 <form action="blogosphericles.php"  method="POST" >
     <textarea  id = "comment" name="comment" rows="35" cols="80">Say something</textarea>
     <br>
@@ -213,4 +218,26 @@ session_start();
 </form>
 </body>
 </html>
+
+<?php 
+
+if ($_SESSION["admin"] == 1)
+{
+    ?>
+<form action="userList.php"  method="POST" >
+    <input id = modd type="submit" value="Mod Users">
+    <input id = "modd" type = "hidden" name = "modd" value = "0">
+</form>
+</body>
+</html>
+
+<?php 
+
+    
+}
+
+?>
+
+
+
 
